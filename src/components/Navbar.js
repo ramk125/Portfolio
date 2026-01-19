7766e49d3fa4ec3fba32a2f40684156968bb6a5e
Ram Kumar <ramk060551@gmail.com>
2026-01-19 20:23:12 +0530

    Fix unused useEffect in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-// import React, { useState } from "react";
 import React, { useState, useEffect } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
